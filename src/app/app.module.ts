852142930ae57e046da9ee6b6c43257402a40f6f
Beth Thomas <bethdianethomas@gmail.com>
2017-12-02 09:05:17 -0700

    routes working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+
 
 const appRoutes: Routes = [
   {
@@ -18,6 +21,16 @@ const appRoutes: Routes = [
     component: BookDetailComponent,
     data: { title: 'Book Details' }
   },
+  {
+    path: 'book-create',
+    component: BookCreateComponent,
+    data: { title: 'Create Book' }
+  },
+  {
+    path: 'book-edit/:id',
+    component: BookEditComponent,
+    data: { title: 'Edit Book' }
+  },
   { path: '',
     redirectTo: '/books',
     pathMatch: 'full'
@@ -29,10 +42,13 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     BookComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-detail',
@@ -12,7 +12,7 @@ export class BookDetailComponent implements OnInit {
 
   book = {};
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
     this.getBookDetail(this.route.snapshot.params['id']);
@@ -22,6 +22,16 @@ export class BookDetailComponent implements OnInit {
     this.http.get('/book/'+id).subscribe(data => {
       this.book = data;
     });
+  }
+
+  deleteBook(id) {
+    this.http.delete('/book/'+id)
+      .subscribe(res => {
+          this.router.navigate(['/books']);
+        }, (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }
